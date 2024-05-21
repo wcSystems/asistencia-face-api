c1223435878e997f2383506bf50851561d5e5573
Anthony Willinthon Carriedo Peña <anthonycarriedo2013@gmail.com>
2024-05-21 15:21:31 -0400

    edit 12

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const fs = require('fs').promises;
 
 const app = express();
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
+    next();
+  });
+  
 app.use(cors({origin: "*"}));
 app.use(express.json());
 app.use(morgan('dev'));
