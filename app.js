6ec881994f6b24f5aa3e2bf6f142e8edd1c2297d
Anthony Willinthon Carriedo Peña <anthonycarriedo2013@gmail.com>
2024-05-30 13:07:54 -0400

    edit 30

--- app.js
+++ app.js
@@ -18,12 +18,6 @@ app.use(cors({origin: "*"}));
     credentials: true
 })); */
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
 app.use(express.json());
 app.use(morgan('dev'));
 
